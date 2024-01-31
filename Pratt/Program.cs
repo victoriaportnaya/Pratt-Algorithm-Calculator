ccba2f4ca75474ce76a5d0710ef386c00cc77c65
Victoria Portnaya <150618993+victoriaportnaya@users.noreply.github.com>
2024-01-31 16:28:50 +0200

    Добавьте файлы проекта.

---
+++ Pratt/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+// set token types and precedences 
+public class Token
+{
+    public enum TokenType
+    {
+        Number, 
+        Operator, 
+        Parenthesis
+    }
+
+    public TokenType Type { get; }
+    public int Value { get; }
+    public char Operator { get; }
+    public static Dictionary<char, int> Precedence = new Dictionary<char, int>
+    {
+        {'+', 1},
+        {"-", 1},
+        {"*", 2},
+        {"/", 2},
+        {")", 0},
+        {"(", 0},
+    };
+
+    private Token(TokenType type, int value, char op)
+    {
+        Type = type; Value = value; Operator = op;
+    }
+
+    public static Token Number(int value) => new Token(TokenType.Number, value);
+    public static Token Operator(char op) => new Token(TokenType.Operator, 0, op);
+    public static Token Parenthesis(char op) => new Token(TokenType.Parenthesis, 0, op);
+
+    public int Precedence => Type == TokenType.Operator ? Precedence.TryGetValue(Operator, out int precedence) ? precedence : 0 : 0;
+
+
+}
+
+// nodes for graph 
+
+// observe syntax tree and parse 
+
+// evaluator 
